7753572b111b3740d2ad38fddad7f8e64a9f6aad
Eolran <nevadasmith147@gmail.com>
2022-10-07 01:33:55 +0100

    add(function to prevent DOM.ready launch)

--- requests.js
+++ requests.js
@@ -1,48 +1,58 @@
-//https://raider.io/api/v1/raiding/hall-of-fame?raid=nyalotha-the-waking-city&difficulty=mythic&region=eu
-var hallOfFame = $.ajax("https://raider.io/api/v1/raiding/hall-of-fame?raid=" + $("#raid") + "&difficulty=" + $("#difficulty") + "&region=" + $("#region"), function () {
-})
-    .done(function (data) {
-        console.log(data);
+function hallOfFame() {
+    //https://raider.io/api/v1/raiding/hall-of-fame?raid=nyalotha-the-waking-city&difficulty=mythic&region=eu
+    var hallOfFame = $.ajax("https://raider.io/api/v1/raiding/hall-of-fame?raid=" + $("#raid") + "&difficulty=" + $("#difficulty") + "&region=" + $("#region"), function () {
     })
-    .fail(function (error) {
-        console.log(error);
-    })
+        .done(function (data) {
+            console.log(data);
+        })
+        .fail(function (error) {
+            console.log(error);
+        })
+}
 
 
-//https://raider.io/api/v1/guilds/profile?region=eu&realm=Hyjal&name=Jetzt%20Gehts%20Los
-var guildInfo = $.ajax("https://raider.io/api/v1/guilds/profile?region=" + $("#region") + "&realm=" + $("#realm") + "&name=" + $("#guildName"), function () {
-})
-    .done(function (data) {
-        console.log(data);
+function Guild() {
+    //https://raider.io/api/v1/guilds/profile?region=eu&realm=Hyjal&name=Jetzt%20Gehts%20Los
+    var guildInfo = $.ajax("https://raider.io/api/v1/guilds/profile?region=" + $("#region") + "&realm=" + $("#realm") + "&name=" + $("#guildName"), function () {
     })
-    .fail(function (error) {
-        console.log(error);
-    })
+        .done(function (data) {
+            console.log(data);
+        })
+        .fail(function (error) {
+            console.log(error);
+        })
 
-//Exemple Guildes pour recherche: Code pour les espaces : %20
-//Jetzt Gehts Los
-//Les tortues du bosquet
-//La Compagnie du Corbeau
-//La Rive Noire
-//
+    //Exemple Guildes pour recherche: Code pour les espaces : %20
+    //Jetzt Gehts Los
+    //Les tortues du bosquet
+    //La Compagnie du Corbeau
+    //La Rive Noire
+    //
+}
 
-//https://raider.io/api/v1/mythic-plus/affixes?region=eu&locale=fr
-var mythicsAffixes = $.ajax("https://raider.io/api/v1/mythic-plus/affixes?region=" + $("#region") + "&locale=en", function () {
-})
-    .done(function (data) {
-        console.log(data);
+function Affixes() {
+    //https://raider.io/api/v1/mythic-plus/affixes?region=eu&locale=fr
+    var mythicsAffixes = $.ajax("https://raider.io/api/v1/mythic-plus/affixes?region=" + $("#region") + "&locale=en", function () {
     })
-    .fail(function (error) {
-        console.log(error);
-    })
+        .done(function (data) {
+            console.log(data);
+        })
+        .fail(function (error) {
+            console.log(error);
+        })
+}
 
 
-//https://raider.io/api/v1/characters/profile?region=eu&realm=Hyjal&name=Eolran
-var charInfo = $.ajax("https://raider.io/api/v1/characters/profile?region=" + $("#region") + "&realm=" + $("#realm") + "&name=" + $("#charName"), function () {
-})
-    .done(function (data) {
-        console.log(data);
+function Character() {
+    //https://raider.io/api/v1/characters/profile?region=eu&realm=Hyjal&name=Eolran
+    var charInfo = $.ajax("https://raider.io/api/v1/characters/profile?region=" + $("#region") + "&realm=" + $("#realm") + "&name=" + $("#charName"), function () {
     })
-    .fail(function (error) {
-        console.log(error);
-    })
+        .done(function (data) {
+            console.log(data);
+        })
+        .fail(function (error) {
+            console.log(error);
+        })
+
+}
+$("#HOF").click(hallOfFame);
